23b374169b9d160346443291ebd0599503f213fc
Robin <robin.django@gmail.com>
2025-05-06 11:19:25 +0200

    feat: Created layout.svelte ✨

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <title>Healthy Urban Living Lab</title>
+  <meta name="description" content="Healthy Urban Living Lab is een living lab van de Hogeschool van Amsterdam, dat samen met haar partners de ontwikkeling van het Bajeskwartier in Amsterdam ondersteunt door middel van onderzoeksprojecten met studenten en partners." />
+</svelte:head>
+
+{@render children()}
