9d3ced71189799a0f9b8738cc1d246cbb12b14a4
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-05-12 11:05:24 +0200

    feat: added homepage query

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,9 +1,73 @@
-/** @type {import('./$types').PageLoad} */
 import getDirectusInstance from '$lib/directus';
 import { readItems } from '@directus/sdk';
 export async function load({ fetch }) {
-	const directus = getDirectusInstance(fetch);
-	return {
-		global: await directus.request(readItems('global')),
-	};
+	try {
+		const directus = getDirectusInstance(fetch);
+		return {
+			projects: await directus.request(readItems('hull_projects', { 
+				filter: {
+					highlighted: { _eq: true }
+				},
+				fields: [
+					'id', 
+					'slug', 
+					'title', 
+					{
+						cover_image: ['id', 'filename_disk', 'description', 'height', 'width']
+					},
+					'description', 
+					'type', 
+					'partners',
+					{
+						images: [{directus_files_id: [
+							'filename_disk', 
+							'description', 
+							'height', 
+							'width'
+						]}]
+					},
+				] 
+			})),
+			partners: await directus.request(readItems('hull_partners', {
+				fields: [
+					'id',
+					'title', 
+					'link',
+					{
+						logo: ['id', 'filename_disk', 'description', 'height', 'width']
+					},
+					'people',
+					'projects'
+				]
+			})),
+			people: await directus.request(readItems('hull_people', {
+				fields: [
+					'id',
+					'name',
+					'position',
+					'link',
+					{
+						image: ['id', 'filename_disk', 'description', 'height', 'width']
+					},
+					'partners'
+				]
+			})),
+			blogposts: await directus.request(readItems('hull_blog', {
+				fields: [
+					'id',
+					'slug',
+					'title',
+					{
+						cover_image: ['id', 'filename_disk', 'description', 'height', 'width']
+					},
+					'content',
+					'publish_date',
+					'authors'
+				]
+			})),
+		};
+	} catch (error) {
+		console.error(error);
+		return {}; // Return empty object if error
+	}
 }
