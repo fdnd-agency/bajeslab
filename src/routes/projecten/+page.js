7a9fbba8dcd1d497d626368511a1a337b74f086d
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-05-12 11:22:06 +0200

    feat: added projects page queries

--- src/routes/projecten/+page.js
+++ src/routes/projecten/+page.js
@@ -1,9 +1,33 @@
-/** @type {import('./$types').PageLoad} */
 import getDirectusInstance from '$lib/directus';
 import { readItems } from '@directus/sdk';
 export async function load({ fetch }) {
-	const directus = getDirectusInstance(fetch);
-	return {
-		global: await directus.request(readItems('global')),
-	};
+	try {
+		const directus = getDirectusInstance(fetch);
+		return {
+			projects: await directus.request(readItems('hull_projects', {
+				fields: [
+					'id', 
+					'slug', 
+					'title', 
+					{
+						cover_image: ['id', 'filename_disk', 'description', 'height', 'width']
+					},
+					'description', 
+					'type', 
+					'partners',
+					{
+						images: [{directus_files_id: [
+							'filename_disk', 
+							'description', 
+							'height', 
+							'width'
+						]}]
+					},
+				] 
+			})),
+		};
+	} catch (error) {
+		console.error(error);
+		return {}; // Return empty object if error
+	}
 }
