96542bb8b02b1dc6b86fcdd4744bf6acf74d6692
Robin <robin.django@gmail.com>
2025-05-12 11:38:11 +0200

    feat: Added query for '/nieuws' 🗃️

--- src/routes/nieuws/[id]/+page.js
+++ src/routes/nieuws/[id]/+page.js
@@ -1,9 +1,25 @@
-/** @type {import('./$types').PageLoad} */
 import getDirectusInstance from '$lib/directus';
 import { readItems } from '@directus/sdk';
 export async function load({ fetch }) {
-	const directus = getDirectusInstance(fetch);
-	return {
-		global: await directus.request(readItems('global')),
-	};
+	try {
+		const directus = getDirectusInstance(fetch);
+		return {
+			posts: await directus.request(readItems('hull_blog', {
+				fields: [
+					'id',
+					'slug',
+					'title',
+					{
+						cover_image: ['id', 'filename_disk', 'description', 'height', 'width']
+					},
+					'content',
+					'publish_date',
+					'authors'
+				]
+			})),
+		};
+	} catch (error) {
+		console.error(error);
+		return {}; // Return empty object if error
+	}
 }
