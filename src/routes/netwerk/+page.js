f6d34a1c9ba7ac98ddd8f994dc272c10b1ee7027
Robin <robin.django@gmail.com>
2025-05-12 11:31:29 +0200

    feat: Added query to '/netwerk/ 🗃️

--- src/routes/netwerk/+page.js
+++ src/routes/netwerk/+page.js
@@ -2,8 +2,24 @@
 import getDirectusInstance from '$lib/directus';
 import { readItems } from '@directus/sdk';
 export async function load({ fetch }) {
-	const directus = getDirectusInstance(fetch);
-	return {
-		global: await directus.request(readItems('global')),
-	};
+	try {
+		const directus = getDirectusInstance(fetch);
+		return {
+			people: await directus.request(readItems('hull_people', {
+				fields: [
+					'id',
+					'name',
+					'position',
+					'link',
+					{
+						image: ['id', 'filename_disk', 'description', 'height', 'width']
+					},
+					'partners'
+				]
+			})),
+		};
+	} catch (error) {
+		console.error(error);
+		return {}; // Return empty object if error
+	}
 }
