84f7e0cf6ce5a93303895655e8b2c9e7e7b87469
Robin <robin.django@gmail.com>
2025-05-15 14:48:19 +0200

    feat: Created ProjectsOverview ✨

---
+++ src/lib/organisms/ProjectsOverview.svelte
@@ -0,0 +1,26 @@
+<script>
+  import OverviewItem from '$lib/molecules/OverviewItem.svelte';
+
+	let { projects } = $props();
+</script>
+
+{#if projects && projects.length > 0}
+	<ul>
+		{#each projects as project (project.id)}
+			<OverviewItem
+				image={project.cover_image}
+        imageAlt={project.title}
+				href={`/projecten/${project.slug}`}
+        itemClass="project-card"
+        contentClass="project-card-content"
+			>
+        <h2>{project.title}</h2>
+        <p>{@html project.description}</p>
+        <p>{project.type}</p>
+        <p>{project.partners}</p>
+      </OverviewItem>
+		{/each}
+	</ul>
+{:else}
+	<p>Geen projecten gevonden. Probeer het later opnieuw.</p>
+{/if}
