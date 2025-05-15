25b52346156c30ff5fac68c3e09b6d6e736e03e5
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-05-15 14:19:40 +0200

    feat: added DirectusImages component

---
+++ src/lib/atoms/DirectusImages.svelte
@@ -0,0 +1,48 @@
+<!-- This component should be used when rendering an image from the Directus backend to automatically optimise it -->
+
+<script>
+	let { 
+    imageId = "", 
+    alt = "Placeholder", 
+    width, 
+    height, 
+    loading = "eager"
+  } = $props()
+
+  const URL = "https://fdnd-agency.directus.app/assets/"
+</script>
+
+{#if imageId === ""}
+<!-- Use placehold.co API to generate a custom placeholder image -->
+	<img
+		src="https://placehold.co/{width}x{height}/c29f9d/3e2518?text=Geen+foto+beschikbaar"
+		{width}
+		{height}
+		{alt}
+		{loading}
+		style="--img-width: {width}; --img-height: {height};"
+	/>
+{:else}
+<!-- Use <picture> to optimise image format using Progressive Enhancement -->
+	<picture>
+		<source type="image/avif" srcset="{URL}{imageId}?fit=cover&width={width}&height={height}&format=avif" />
+		<source type="image/webp" srcset="{URL}{imageId}?fit=cover&width={width}&height={height}&format=webp" />
+		<img
+			src="{URL}{imageId}?fit=cover&width={width}&height={height}&format=png"
+			{width}
+			{height}
+			{alt}
+			{loading}
+			style="--img-width: {width}; --img-height: {height}"
+		/>
+	</picture>
+{/if}
+
+<style>
+	img {
+		width: 100%;
+		height: auto;
+		aspect-ratio: var(--img-width) / var(--img-height);
+		border-radius:var(--border-radius-md);
+	}
+</style>
