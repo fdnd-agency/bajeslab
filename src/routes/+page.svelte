eac3f0597ba1c69569627174272442f092523d23
Saif8599 <78470780+Saif8599@users.noreply.github.com>
2025-10-14 23:58:02 +0200

    feat: import Themalijnen in page#112

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-    import Hero from '$lib/components/homepage/Hero.svelte';
+	import Hero from '$lib/components/homepage/Hero.svelte';
+	import Themalijnen from '$lib/components/homepage/Themalijnen.svelte';
 </script>
 
 <Hero />
+<Themalijnen />
