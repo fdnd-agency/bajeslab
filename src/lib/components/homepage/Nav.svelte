bf2ee182cd43b328ca6a27ee4dd20f89c1ede804
Naddybsx <Nadd_bs@icloud.com>
2025-10-17 16:30:10 +0200

    feat: implemented Popover API for navigation toggle #156

--- src/lib/components/homepage/Nav.svelte
+++ src/lib/components/homepage/Nav.svelte
@@ -4,8 +4,9 @@
 
 <header>
   <a href="/"><span>Healthy Urban</span>Living Lab</a>
-
-  <nav>
+  <button popovertarget="menu">☰</button>
+  <nav id="menu" popover>
+    <button popovertarget="menu" popovertargetaction="hide">✕</button>  
       <ul>
         <li><a href="/over-ons">Over ons</a></li>
         <li><a href="/projecten">Projecten</a></li>
