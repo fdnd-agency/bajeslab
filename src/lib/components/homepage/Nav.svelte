1f80a530c10ef600d5ddfe9dfd774138369dc4f3
Naddybsx <Nadd_bs@icloud.com>
2025-10-17 06:30:08 +0200

    feat: added semantic HTML structure for main navigation (component) #129

---
+++ src/lib/components/homepage/Nav.svelte
@@ -0,0 +1,20 @@
+<script>
+// ...
+</script>
+
+<header>
+  <a href="/"><span>Healthy Urban</span>Living Lab</a>
+
+  <nav>
+      <ul>
+        <li><a href="/over-ons">Over ons</a></li>
+        <li><a href="/projecten">Projecten</a></li>
+        <li><a href="/netwerk">Netwerk</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+  </nav>
+</header>
+
+<style>
+/* ... */
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script>
   let { children } = $props();
+  import Nav from '$lib/components/homepage/Nav.svelte';
 </script>
-
+<Nav />
 <svelte:head>
   <title>Healthy Urban Living Lab</title>
   <meta name="description" content="Healthy Urban Living Lab is een living lab van de Hogeschool van Amsterdam, dat samen met haar partners de ontwikkeling van het Bajeskwartier in Amsterdam ondersteunt door middel van onderzoeksprojecten met studenten en partners." />
