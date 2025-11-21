47c066343d70059580ffaa9309a8500f72d4a397
Saif8599 <78470780+Saif8599@users.noreply.github.com>
2025-11-21 20:59:39 +0100

    feat: add storybook story #142

---
+++ src/lib/components/organisms/blocks/ThemalijnenBlock/ThemalijnenBlock.stories.svelte
@@ -0,0 +1,28 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import ThemalijnenBlock from './ThemalijnenBlock.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Organisms/Blocks/ThemalijnenBlock',
+		component: ThemalijnenBlock,
+		tags: ['autodocs'],
+		argTypes: {
+			title: {
+				control: 'text'
+			}
+		},
+		parameters: {
+			docs: {
+				description: {
+					component: ` 
+					
+					`
+				}
+			}
+		}
+	});
+</script>
+
+<Story
+	name="Standaard"
+/>
