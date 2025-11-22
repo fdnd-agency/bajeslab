a3abf291aad2855d149952c7ac3fa6c98f37ac22
Saif8599 <78470780+Saif8599@users.noreply.github.com>
2025-11-22 19:21:00 +0100

    fix: resolve scoped css styling in storybook #142

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,18 @@
-
-
 /** @type { import('@storybook/sveltekit').StorybookConfig } */
 const config = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|ts|svelte)"
-  ],
-  "addons": [
-    "@storybook/addon-svelte-csf",
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
-  ],
-  "framework": {
-    "name": "@storybook/sveltekit",
-    "options": {}
-  }
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts|svelte)'],
+	addons: [
+		'@storybook/addon-svelte-csf',
+		'@chromatic-com/storybook',
+		'@storybook/addon-docs',
+		'@storybook/addon-a11y',
+		'@storybook/addon-vitest'
+	],
+	framework: {
+		name: '@storybook/sveltekit',
+		options: {}
+	},
+	// Path naar CSS folder
+	staticDirs: ['../static/css']
 };
 export default config;
