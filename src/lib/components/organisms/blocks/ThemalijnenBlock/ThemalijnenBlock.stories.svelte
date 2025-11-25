dd6c1252f97815be91ad6f5ae521017f1559a687
Saif8599 <78470780+Saif8599@users.noreply.github.com>
2025-11-25 13:49:21 +0100

    feat: add figma design #142

--- src/lib/components/organisms/blocks/ThemalijnenBlock/ThemalijnenBlock.stories.svelte
+++ src/lib/components/organisms/blocks/ThemalijnenBlock/ThemalijnenBlock.stories.svelte
@@ -16,6 +16,10 @@
 			}
 		},
 		parameters: {
+			design: {
+				type: 'figma',
+				url: 'https://www.figma.com/design/doguH52moO2cyA3pT0B6sd/Living-Lab-Bajeskwartier-2025-2026?node-id=3-120&t=IeBjcBYWen78cWmX-1'
+			},
 			docs: {
 				description: {
 					component: `
