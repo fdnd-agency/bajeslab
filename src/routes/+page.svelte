023185cc31c2cd120b1e9715c6169e00bb3db378
Saif8599 <78470780+Saif8599@users.noreply.github.com>
2025-11-28 12:24:11 +0100

    chore: use ThemelinesBlock correctly in over-ons #142

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
 	import HeroBlock from '$lib/components/organisms/blocks/HeroBlock.svelte';
 	import Overons from '$lib/components/organisms/blocks/Overons.svelte';
-	import ThemalijnenBlock from '$lib/components/organisms/blocks/ThemelinesBlock/ThemelinesBlock.svelte';
 	import ProjectenBlock from '$lib/components/organisms/blocks/ProjectenBlock.svelte';
 	import NieuwsbriefBlock from '$lib/components/organisms/blocks/NieuwsbriefBlock.svelte';
+	import ThemelinesBlock from '$lib/components/organisms/blocks/ThemelinesBlock/ThemelinesBlock.svelte';
 
 	export let data; // komt van +page.server.js
 	const { projects, themes } = data;
@@ -16,7 +16,7 @@
 	description="In de voormalige Bijlmer Bajes werken gebiedsontwikkelaar AM, de HvA en verschillende partners sinds 2018 samen in het Healthy Urban Living Lab Bajeskwartier. Het doel is een groene, gezonde en inclusieve buurt voor alle bewoners. Onderzoekers en studenten bekijken samen met bewoners en gebruikers hoe het gebied zo ingericht kan worden dat het bewegen en ontmoeten stimuleert. Dit is belangrijk voor gezondheid en welzijn ..."
 	image="/images/over-ons.png"
 />
-<ThemalijnenBlock
+<ThemelinesBlock
 	{themes}
 	buttonText="Lees meer"
 	buttonLink="/over-ons#theme-line"
--- src/routes/over-ons/+page.svelte
+++ src/routes/over-ons/+page.svelte
@@ -1,15 +1,17 @@
 <script>
-	import ThemalijnenBlock from '$lib/components/organisms/blocks/ThemelinesBlock/ThemelinesBlock.svelte';
+	import ThemelinesBlock from '$lib/components/organisms/blocks/ThemelinesBlock/ThemelinesBlock.svelte';
 
 	export let data; // komt van +page.server.js
 	const { themes } = data;
 </script>
 
-<ThemalijnenBlock
+
+<ThemelinesBlock
 	{themes}
 	title="Themalijnen"
 	layout="column"
 	showIntro
+	showButton={false}
 	buttonText="Lees meer"
 	buttonPosition="flex-end"
 	buttonLink="/over-ons#theme-line"
