ea5d5a1ec28c1064856a2c8fa940a81b5b9f2f7d
Brancovanbeek <branco.van.beek@hva.nl>
2025-12-09 11:03:34 +0100

    feat: Arrow.icon changes

--- src/lib/components/atoms/icons/ArrowIcon.svelte
+++ src/lib/components/atoms/icons/ArrowIcon.svelte
@@ -1,38 +1,41 @@
 <script>
-	let { className = '' } = $props();
-  </script>
+	let { 
+		className = ''
+	 } = $props();
+</script>
   
-  <svg
+<svg
 	width="14"
 	height="22"
 	viewBox="0 0 14 22"
 	fill="currentColor"
 	xmlns="http://www.w3.org/2000/svg"
 	class={className}
-  >
+>
 	<rect
-	  x="1"
-	  y="19.6067"
-	  width="15.0768"
-	  height="3"
-	  transform="rotate(-45 1 19.6067)"
-	  fill="currentColor"
+		x="1"
+		y="19.6067"
+		width="15.0768"
+		height="3"
+		transform="rotate(-45 1 19.6067)"
+		fill="currentColor"
 	/>
 	<path
-	  d="M3.10645 0.393799L13.713 11.0004L11.5917 13.1217L0.985125 2.51512L3.10645 0.393799Z"
-	  fill="currentColor"
+		d="M3.10645 0.393799L13.713 11.0004L11.5917 13.1217L0.985125 2.51512L3.10645 0.393799Z"
+		fill="currentColor"
 	/>
-  </svg>
+</svg>
   
-  <style>
+<style>
 	svg {
 	  display: inline-block;
 	  vertical-align: middle;
 	}
-	
-	.arrow {
+
+	.project-arrow {
 	  width: 0.8rem;
 	  height: 0.8rem;
-	  margin-left: 1rem;
+	  margin-left: 0.5rem;
 	}
-  </style>
+</style>
+  
