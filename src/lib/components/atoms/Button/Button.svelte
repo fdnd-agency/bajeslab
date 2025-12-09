386b952c47cf293e52d67c654c249ef6c18eac4e
Brancovanbeek <branco.van.beek@hva.nl>
2025-12-09 11:02:55 +0100

    feat: Button.svelte changes

--- src/lib/components/atoms/Button/Button.svelte
+++ src/lib/components/atoms/Button/Button.svelte
@@ -1,24 +1,40 @@
 <script>
 	// Fallback value
-	let { text = 'Lees meer', href = '', position = 'center', className = '' } = $props();
+	let { 
+		text = 'Lees meer', 
+		href = '', 
+		position = 'center', 
+		className = '',
+		reload = false,
+		direction = 'next'
+	} = $props();
 
 	import ArrowIcon from '../icons/ArrowIcon.svelte';
-  </script>
+</script>
   
-  <a class="button {position} {className}"{href}{...(reload ? {'data-sveltekit-reload': true} : {})}>
-	{text} <ArrowIcon />
-  </a>
+<a 
+  class="button button--{direction} {position} {className}"
+  {href}
+  {...(reload ? {'data-sveltekit-reload': true} : {})}
+>
+  {text}
+  <ArrowIcon class={className} />
+</a>
   
-  <style>
-	/* Button positions */
-	.flex-start {
-		align-self: flex-start;
+<style>
+	.button {
+		display: inline-flex;
+		align-items: center;
+		gap: 0.5rem;
+		text-decoration: none;
 	}
-	.center {
-		align-self: center;
+
+	.project-arrow {
+		width: 0.8rem;
+		height: 0.8rem;
 	}
-	.flex-end {
-		align-self: flex-end;
-	}
-  </style>
-  
+
+	.flex-start { align-self: flex-start; }
+	.center { align-self: center; }
+	.flex-end { align-self: flex-end; }
+</style>
