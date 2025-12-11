fc008452a7fe1b09bbd6475902a202259f37d622
Brancovanbeek <branco.van.beek@hva.nl>
2025-12-11 20:44:01 +0100

    feat: add html

---
+++ src/routes/spike/+page.svelte
@@ -0,0 +1,40 @@
+<script></script>
+
+<div class="page page-1">
+    <h1 class="h1-1">Scroll naar beneden</h1>
+    <img class="img-1" src="/images/mystic/mg-ade-tree.0x640.webp" alt="boom">
+    <img class="img-2" src="/images/mystic/wgf-sidetree3-left.0x640.webp" alt="boom">
+    <img class="img-dot1" src="/images/mystic/mg-story-background2.1920x0.webp" alt="dot">
+    <img class="img-dot2" src="/images/mystic/mg-story-background2.1920x0.webp" alt="dot">
+</div>
+
+<section class="horizontal">
+    <div class="sticky">
+      <div class="split-image"></div> 
+      <div class="scroll-container">
+        <div class="page page-2">
+          <h1>Horizontal 1</h1>
+          <img class="img-rozeboom2" src="/images/mystic/wgf-sidetree3-left.0x640.webp" alt="boom">
+          <img class="img-3" src="/images/mystic/mg-tree-right1.0x640.webp" alt="boom">
+        </div>
+        <div class="page page-3">
+          <h1>Horizontal 2</h1>
+          <img class="img-rodeboom" src="/images/mystic/mg-ade-tree.0x640.webp" alt="boom">
+          <img class="img-rozeboom" src="/images/mystic/wgf-sidetree3-left.0x640.webp" alt="boom">
+          <img class="img-4" src="/images/mystic/mystic-garden-festival-website-video-foto.1920x0x0x100.webp" alt="foto van festival">
+        </div>
+        <div class="page page-4">
+          <h1>Horizontal 3</h1>
+          <img class="img-rodeboom2" src="/images/mystic/mg-ade-tree.0x640.webp" alt="boom">
+          <img class="img-5" src="/images/mystic/blurry-view-gaze-motion-forward-600nw-2492883561.jpg.webp" alt="foto van brug">
+          <img class="img-6" src="/images/mystic/mg-header-logo2.1920x0x0x100.webp" alt="logo">
+        </div>
+      </div>
+    </div>
+</section>  
+
+<div class="page page-5">
+    <img class="img-7" src="/images/mystic/mg-tree-right1.0x640.webp" alt="boom">
+    <img class="img-8" src="/images/mystic/mg-divider-footer-10-2bg.1920x0.webp" alt="poort">
+</div>
+
