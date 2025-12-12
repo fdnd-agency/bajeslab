12ca14b7dcfdb3781800bf8260890242e91ee70a
Saif8599 <78470780+Saif8599@users.noreply.github.com>
2025-12-12 18:35:46 +0100

    feat: add content layer #221

--- src/lib/components/organisms/blocks/CardComponent.svelte
+++ src/lib/components/organisms/blocks/CardComponent.svelte
@@ -8,12 +8,21 @@
 			<li class="raster-cel"></li>
 		{/each}
 	</ul>
+	<div class="content-layer">
+		<h1 class="card-title">DECRYPT PULSE ECHO</h1>
+		<p class="card-description">
+			Status: Signal Intercepted. Unstable data packets detected. Proceed to unlock the component's
+			corrupted core.
+		</p>
+	</div>
 </section>
 
 <style>
 	section {
 		--grid-size: 30;
 		--neon-rgb: 139, 248, 16;
+		--green-rgb: 83, 253, 112;
+		--white-rgb: 255, 255, 255;
 
 		background-color: #0d1f3b;
 		width: 100vw;
@@ -42,5 +51,53 @@
 	.raster-cel:hover {
 		background-color: rgba(var(--neon-rgb), 0.4);
 		transition-duration: 0.02s;
+	}
+
+	.content-layer {
+		position: relative;
+		width: 100%;
+		max-width: 800px;
+		margin: 0 auto;
+
+		height: auto;
+		z-index: 10;
+
+		padding: 2rem;
+		border: 5px solid rgba(var(--green-rgb), 1);
+
+		text-shadow: 0 0 5px rgba(var(--neon-rgb), 0.7);
+
+		color: rgb(var(--neon-rgb));
+		text-shadow: 0 0 5px rgba(var(--neon-rgb));
+		border: 5px solid rgba(var(--green-rgb));
+	}
+
+	.card-title {
+		color: rgb(var(--green-rgb));
+		font-size: 2.2em;
+		margin-bottom: 0.5em;
+		font-family: monospace;
+	}
+
+	.card-description {
+		color: rgb(var(--white-rgb));
+		font-size: 1.2em;
+		max-width: 500px;
+		line-height: 1.5;
+		font-family: monospace;
+	}
+
+	@media (max-width: 768px) {
+		.content-layer {
+			padding: 1rem;
+		}
+
+		.card-title {
+			font-size: 1.8rem;
+		}
+
+		.card-description {
+			font-size: 1rem; 
+		}
 	}
 </style>
