dca9d97882cacb8bf0dc67c6aa2103d9b815c6d6
Brancovanbeek <branco.van.beek@hva.nl>
2025-12-16 20:06:46 +0100

    feat: add general contact information, #147 #150

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,2 +1,66 @@
-<h1>Contact</h1>
-<p>Hier komt de contact pagina</p>
+<script>
+    import ContactCard from "$lib/components/molecules/ContactCard/ContactCard.svelte";
+    let { 
+        email = "brancovanbeek5@gmail.com",
+        nummer =  "0639646635",
+        title = "Contact", 
+        data 
+    } = $props();
+    let { contact } = data;
+</script>
+
+<section class="container">
+    <h1 class="title">{title}</h1>
+
+    <section class="info">
+        <p class="text">
+            Heeft u vragen over ons werk of projecten? Neem gerust contact met ons op.
+        </p>
+
+        <p class="text">
+            Algemene e-mail:
+            <a href="mailto:{email}" class="email">{email}</a>
+        </p>
+
+        <p class="text">
+            Algemene nummer:
+            <a href="tel:{nummer}" class="number">{nummer}</a>
+        </p>
+    </section>
+
+    <section class="contact-cards">
+        {#each data.contacts as contact}
+            <ContactCard {contact} />
+        {/each}
+    </section>
+</section>
+
+<style>
+    .email, .number{
+        font-weight: bold;
+
+        &:hover{
+            text-decoration: underline;
+        }
+    }
+    .info{
+        margin-bottom: var(--spacing-lg);
+        margin-top: var(--spacing-lg);
+        p{
+            margin-bottom: var(--spacing-sm);
+        }
+    }
+    .contact-cards{
+        container-type: inline-size;
+        display: grid;
+        grid-template-columns: 1fr;
+        gap: var(--spacing-lg);
+        margin-bottom: var(--spacing-lg);
+
+        @media (min-width: 1200px) {
+            grid-template-columns: repeat(2, 1fr);
+            gap: var(--spacing-lg);
+            
+        }
+    }
+</style>
